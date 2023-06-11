2c6e236ea52353d3e3610218e21bb824cf706578
VikBrains <zi3gglhdjfk9@mail.ru>
2023-06-11 20:29:14 +0300

    add task chapters

--- Seminar_1/Seminar_1/ConsolApp1/Program.cs
+++ Seminar_1/Seminar_1/ConsolApp1/Program.cs
@@ -1,6 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
 
-//Получение числа от юзера
+
+Console.WriteLine("Input first integer: ");
+int number_1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input second integer: ");
+int number_2 = int.Parse(Console.ReadLine());
+
+int number_3 = number_2 * number_2;
+
+if (number_1 == number_3)
+{
+    Console.WriteLine("The first number is the square of the second number");
+}
+else
+{
+    Console.WriteLine("The first number is NOT the square of the second number");
+}
 
 
-Console.WriteLine(5 % 10);
+
+// Console.WriteLine(result);
