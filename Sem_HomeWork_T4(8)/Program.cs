ad197839998a340c35178a65ec7c87de25926ddf
VikBrains <zi3gglhdjfk9@mail.ru>
2023-06-12 20:34:45 +0300

    add solution of Task4(8)

--- Sem_HomeWork_T4(8)/Program.cs
+++ Sem_HomeWork_T4(8)/Program.cs
@@ -3,14 +3,14 @@
 // Пример: "5" -> 2, 4   /  "8" -> 2, 4, 6, 8
 
 Console.Write("Input your integer: ");
-int num = int.Parse(Console.ReadLine());
+int numMax = int.Parse(Console.ReadLine());
+Console.WriteLine(numMax);
 
-int[] nums = new int[num];
-for (int i = 0;)
+int num = 2;
+
+while (num < numMax)
 {
-    
+    Console.Write(num);
+    Console.Write(" ");
+    num = num + 2; 
 }
-
-numEn = 0 
-
-int numen = (num % 2);
