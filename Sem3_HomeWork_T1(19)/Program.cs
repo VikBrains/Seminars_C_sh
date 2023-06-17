d0e050b3f57f0b90747a0b3b22e6ee24f9f85f89
VikBrains <zi3gglhdjfk9@mail.ru>
2023-06-17 19:15:04 +0300

    try Method T19

--- Sem3_HomeWork_T1(19)/Program.cs
+++ Sem3_HomeWork_T1(19)/Program.cs
@@ -1,13 +1,23 @@
 ﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число
 // и проверяет, является ли оно палиндромом.
 
+// double number = GetTurn(number)
+// int result = 0;
+// while(number > 0)
+//{
+//    result = result*10;
+//    result = result + number % 10;
+//    number = number / 10;
+//    return result;
+//}
+
+
 Console.Write("Input your number: ");
 int number = int.Parse(Console.ReadLine());
 int num_0 = number;
 
 Console.WriteLine(num_0);
 
-// int result = GetTurn(number);
 
 int result = 0;
 
