274c3c8c7b5bd4b98d310d79a3d7b1c253aaa0aa
VikBrains <zi3gglhdjfk9@mail.ru>
2023-06-17 17:04:25 +0300

    add solution T21 & T23

---
+++ Sem3_HomeWork_T2(21)/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+// это 2D:
+Console.Write("Input coordinates x1: ");
+int ax = int.Parse(Console.ReadLine());
+
+Console.Write("Input coordinates y1: ");
+int ay = int.Parse(Console.ReadLine());
+
+Console.Write("Input coordinates z1: ");
+int az = int.Parse(Console.ReadLine());
+
+Console.Write("Input coordinates x2: ");
+int bx = int.Parse(Console.ReadLine());
+
+Console.Write("Input coordinates y2: ");
+int by = int.Parse(Console.ReadLine());
+
+Console.Write("Input coordinates z2: ");
+int bz = int.Parse(Console.ReadLine());
+
+// double result = Math.Round(Math.Cbrt(Math.Pow((bx-ax),2) + Math.Pow((by-ay),2) + Math.Pow((bz-az),2)),3);
+Console.WriteLine($"Distanse beetwin points is: {Math.Cbrt(Math.Pow((bx-ax),2) + Math.Pow((by-ay),2) + Math.Pow((bz-az),2))}");
---
+++ Sem3_HomeWork_T3(23)/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N:  3 -> 1, 4, 9. / 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Input digit: ");
+int a = int.Parse(Console.ReadLine());
+int i = 0;
+
+while (i <= a)
+{
+    Console.Write($"{i} ^ 3 = ");
+    Console.WriteLine(Math.Pow(i,3));
+    i = i + 1;
+}
