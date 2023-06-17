f7bac862efb9bd5b06012afd440ecce4c0120fb8
VikBrains <zi3gglhdjfk9@mail.ru>
2023-06-17 18:52:18 +0300

    add Method to Task 21

--- Sem3_HomeWork_T1(19)/Program.cs
+++ Sem3_HomeWork_T1(19)/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+
+Console.Write("Input your number: ");
+int number = int.Parse(Console.ReadLine());
+int num_0 = number;
+
+Console.WriteLine(num_0);
+
+// int result = GetTurn(number);
+
+int result = 0;
+
+while(number > 0)
+{
+    result = result*10;
+    result = result + number % 10;
+    number = number / 10;
+}
+Console.WriteLine(result);
+
+if(num_0 == result)
+{
+    Console.WriteLine("yes, palindrome");
+}
+else
+{
+    Console.WriteLine("Not");
+}
--- Sem3_HomeWork_T2(21)/Program.cs
+++ Sem3_HomeWork_T2(21)/Program.cs
@@ -1,24 +1,32 @@
 ﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек 
 // и находит расстояние между ними в 3D пространстве.
 
-// это 2D:
-Console.Write("Input coordinates x1: ");
+double GetDistance3D(int ax, int ay, int az, int bx, int by, int bz)
+{
+    double result = Math.Round(Math.Cbrt(Math.Pow((bx-ax),2) + Math.Pow((by-ay),2) + Math.Pow((bz-az),2)),3);
+    return result;
+}
+
+Console.Write("Input coordinate x1: ");
 int ax = int.Parse(Console.ReadLine());
 
-Console.Write("Input coordinates y1: ");
+Console.Write("Input coordinate y1: ");
 int ay = int.Parse(Console.ReadLine());
 
-Console.Write("Input coordinates z1: ");
+Console.Write("Input coordinate z1: ");
 int az = int.Parse(Console.ReadLine());
 
-Console.Write("Input coordinates x2: ");
+Console.Write("Input coordinate x2: ");
 int bx = int.Parse(Console.ReadLine());
 
-Console.Write("Input coordinates y2: ");
+Console.Write("Input coordinate y2: ");
 int by = int.Parse(Console.ReadLine());
 
-Console.Write("Input coordinates z2: ");
+Console.Write("Input coordinate z2: ");
 int bz = int.Parse(Console.ReadLine());
 
-// double result = Math.Round(Math.Cbrt(Math.Pow((bx-ax),2) + Math.Pow((by-ay),2) + Math.Pow((bz-az),2)),3);
-Console.WriteLine($"Distanse beetwin points is: {Math.Cbrt(Math.Pow((bx-ax),2) + Math.Pow((by-ay),2) + Math.Pow((bz-az),2))}");
+Console.WriteLine(GetDistance3D(ax, ay, az, bx, by, bz));
+
+// без Метода:
+Console.WriteLine($"Distanse beetwin points is: {Math.Cbrt(Math.Pow((bx-ax),2) + Math.Pow((by-ay),2) + Math.Pow((bz-az),2))}"); 
+
