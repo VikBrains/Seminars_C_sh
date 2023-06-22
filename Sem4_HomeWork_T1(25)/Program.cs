3b63c04999c1dede05624020861f4fd686bb1b60
VikBrains <zi3gglhdjfk9@mail.ru>
2023-06-22 23:58:03 +0300

    add tasks omeork_4

---
+++ Sem4_HomeWork_T1(25)/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+Console.Write("Input your number: ");
+int num = int.Parse(Console.ReadLine());
+
---
+++ Sem4_HomeWork_T2(27)/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Input your number: ");
+int number = int.Parse(Console.ReadLine());
+
+int num_0 = Convert.ToInt32(Math.Abs(number));
+int i = 0;
+int sum = 0;
+
+while (num_0 > i)
+{
+    int num_1 = num_0 / 10;
+    Console.WriteLine(num_1);
+    num_0 = num_0 % 10;
+    Console.WriteLine(num_0);
+    sum = sum + num_0;
+    Console.WriteLine(sum);
+    num_0 = num_1;
+    Console.WriteLine($"return _0 {num_0}");
+    i++;
+}
+Console.WriteLine(sum);
