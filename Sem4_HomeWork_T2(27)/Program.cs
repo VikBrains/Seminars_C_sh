9a6f47a2be9803c54448248c96e3047f3c20297b
VikBrains <zi3gglhdjfk9@mail.ru>
2023-06-23 23:19:57 +0300

    add new task Sem_5

--- Sem4_HomeWork_T2(27)/Program.cs
+++ Sem4_HomeWork_T2(27)/Program.cs
@@ -7,16 +7,12 @@ int num_0 = Convert.ToInt32(Math.Abs(number));
 int i = 0;
 int sum = 0;
 
-while (num_0 > i)
+while (num_0 > 0)
 {
     int num_1 = num_0 / 10;
-    Console.WriteLine(num_1);
     num_0 = num_0 % 10;
-    Console.WriteLine(num_0);
     sum = sum + num_0;
-    Console.WriteLine(sum);
     num_0 = num_1;
-    Console.WriteLine($"return _0 {num_0}");
     i++;
 }
 Console.WriteLine(sum);
---
+++ Seminar_5_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// адача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+Console.Write("Input your number: ");
+int num = int.Parse(Console.ReadLine());
+
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 10);
+    Console.WriteLine($"{array[i]} ");
+}
+Console.WriteLine(" ");
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (num == array[i])
+    {
+        // Console.WriteLine($"{array[i]} ");
+        Console.Write("There is num in array");
+        // break;
+    }
+    else
+        Console.Write("there is not num in array");
+        break;
+}    
