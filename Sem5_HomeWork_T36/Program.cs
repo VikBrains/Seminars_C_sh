e989e9f56ff0a97d7f00c9f3f6ef22af764e205a
VikBrains <zi3gglhdjfk9@mail.ru>
2023-06-26 23:50:09 +0300

    add solutions for omWork_5

--- Sem5_HomeWork_T36/Program.cs
+++ Sem5_HomeWork_T36/Program.cs
@@ -2,4 +2,32 @@
 // Найдите сумму элементов, стоящих на нечётных позициях.
 //  [3, 7, 23, 12] -> 19    [-4, -6, 89, 6] -> 0
 
+void FillArray(int[] array)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10,20);
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
 
+Console.WriteLine("Input array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+
+Console.WriteLine(" -- ");
+Console.Write("Sum of odd in Array  ");
+
+FillArray(array);
+
+int sum = 0;
+
+for (int j = 1; j < array.Length; j+=2)
+{
+    sum = sum + array[j];
+}
+
+Console.WriteLine($" > {sum} ");
+Console.WriteLine(" -- ");
--- Sem5_HomeWork_T38/Program.cs
+++ Sem5_HomeWork_T38/Program.cs
@@ -1,4 +1,54 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
+//void FillArray(double[] numbers)
+//{
+//    Random rand = new Random();
+//    for (int i = 0; i < numbers; i++)
+//    {
+//        numbers[i] = rand.NextDouble(-100,100);
+//        Console.Write(numbers[i] + " ");
+//    }
+//    Console.WriteLine();
+//}
 
+void FillArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Input array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+double[] numbers = new double[size];
+
+Console.WriteLine("Array are: ");
+FillArray(numbers);
+Console.WriteLine(" -- ");
+
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int j = 0; j < numbers.Length; j++)
+{
+    if (numbers[j] > max)
+        {
+            max = numbers[j];
+        }
+    if (numbers[j] < min)
+        {
+            min = numbers[j];
+        }
+}
+
+Console.WriteLine($"Total {numbers.Length} numbers.");
+Console.WriteLine($"Max value = {max}, Min value = {min}");
+Console.WriteLine($"Difference max - min = {max - min}");
+Console.WriteLine(" -- ");
