68cffb203b1d86d1a74057b53904ea27a9377b77
VikBrains <zi3gglhdjfk9@mail.ru>
2023-07-16 22:40:43 +0300

    add T64 & T66

---
+++ Sem9_HomeWork_T64/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+
+Console.Write("Input integer: ");
+int numM = int.Parse(Console.ReadLine());
+
+int numN = 1;
+string PrintNumbers(int start, int end)
+{
+    if (start == 1) return start.ToString();
+    return (start + " " + PrintNumbers(start - 1, end));
+}
+Console.WriteLine(PrintNumbers(numM, numN));
---
+++ Sem9_HomeWork_T68/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 29*/
+
+Console.Write("Input integer: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Input integer: ");
+int n = int.Parse(Console.ReadLine());
+
+
